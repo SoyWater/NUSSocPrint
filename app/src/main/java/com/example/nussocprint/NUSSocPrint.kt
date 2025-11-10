3903e26417dfd17256c74f8f76d17db068b7f448
SoyWater <153174763+SoyWater@users.noreply.github.com>
2025-11-10 19:46:36 +0800

    Add printJob handling

--- app/src/main/java/com/example/nussocprint/NUSSocPrint.kt
+++ app/src/main/java/com/example/nussocprint/NUSSocPrint.kt
@@ -1,22 +1,72 @@
 package com.example.nussocprint
 
+import android.os.ParcelFileDescriptor
 import android.printservice.PrintJob
 import android.printservice.PrintService
 import android.printservice.PrinterDiscoverySession
 import android.util.Log
+import com.example.nussocprint.util.EncryptedDataStore
+import com.example.nussocprint.util.SmbjUtils.printStream
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import java.io.FileInputStream
+
+private const val TAG: String = "SoCPrintService"
+
+const val HOST: String = "nts27.comp.nus.edu.sg"
+const val DOMAIN: String = "nusstu"
 
 class NUSSocPrint : PrintService() {
-    private val TAG: String = "MyPrinterDiscovery"
+
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
     override fun onCreatePrinterDiscoverySession(): PrinterDiscoverySession {
-        Log.d(TAG, "onCreatePrinterDiscoverySession")
+        Log.d(TAG, "onCreatePrinterDiscoverySession() - returning SoCDiscoverySession")
         return SoCDiscoverySession(this)
     }
 
-    override fun onPrintJobQueued(printJob: PrintJob?) {
-        TODO("Not yet implemented")
+    override fun onPrintJobQueued(printJob: PrintJob) {
+        Log.d(TAG, "onPrintJobQueued() - new print job: $printJob")
+        scope.launch {
+            printJob.start()
+
+            // Get credentials
+            val credentials = EncryptedDataStore.getCredentials(applicationContext)
+            if (credentials == null) {
+                Log.d(TAG, "No credentials found")
+                return@launch
+            }
+
+            // Get printer name
+            val printerName = printJob.info.printerId?.localId
+            if (printerName == null) {
+                Log.d(TAG, "No printer found")
+                return@launch
+            }
+            Log.d(TAG, "Printing to $printerName")
+
+            // Parse file into stream and pass to printer
+            printJob.document.data.use { pfd ->
+                FileInputStream(pfd?.fileDescriptor).use { stream ->
+                    printStream(
+                        HOST,
+                        DOMAIN,
+                        credentials.username,
+                        credentials.password,
+                        printerName,
+                        stream
+                    )
+                }
+            }
+
+        }
+
     }
 
     override fun onRequestCancelPrintJob(printJob: PrintJob) {
         printJob.cancel()
     }
 }
+
